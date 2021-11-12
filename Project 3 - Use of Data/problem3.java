99020460159db9dfa3140025d70322c2fba7dafa
paulardis <85268281+pabloisaiahs@users.noreply.github.com>
2021-11-12 17:09:18 -0500

    Update problem3.java

--- Project 3 - Use of Data/problem3.java
+++ Project 3 - Use of Data/problem3.java
@@ -1,3 +1,9 @@
+#Write a program that asks the user to input:
+#The number of gallons of gas in the tank
+#The fuel efficiency in miles per gallon
+#The price of gas per gallon
+#Then print the cost per 100 miles and how far the car can go with the gas in the car.
+     
 import java.util.Scanner;
 public class problem3{
 
